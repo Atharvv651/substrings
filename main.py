474ef01fadc4b6708c6bd0548ed86a3a117f73ec
Atharvv651 <bhawna.tandon@gmail.com>
2024-10-26 18:36:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def all_substrings(s):
+    substrings = []
+    length = len(s)
+    for i in range(length):
+        for j in range(i + 1, length + 1):
+            substrings.append(s[i:j])
+    return substrings
+
+string = input("Enter a string: ")
+result = all_substrings(string)
+print("All substrings are:", result)
